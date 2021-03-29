dde5819f08c5847da8f8bf49b6ce0deaeb15fc5c
ShehabEl-DeenAlalkamy <essamshehab33_3@h-eng.helwan.edu.eg>
2021-03-29 21:57:01 +0200

    Reverting to sqllite3 db

--- egyfund/settings.py
+++ egyfund/settings.py
@@ -73,16 +73,22 @@ WSGI_APPLICATION = 'egyfund.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'egyfund_schema',
+#         'USER': 'admin',
+#         'PASSWORD': 'ggizi',
+#         'HOST': '127.0.0.1',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'egyfund_schema',
-        'USER': 'admin',
-        'PASSWORD': 'ggizi',
-        'HOST': '127.0.0.1',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
