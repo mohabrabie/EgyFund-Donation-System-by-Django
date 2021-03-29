400bcfc468817d67b9722b4842f09d29135c1f84
ahmedKhaled1995 <bigbossak84@gmail.com>
2021-03-30 01:55:16 +0200

    Fixed clashing migrations

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,9 +1,5 @@
 from django.contrib import admin
 from .models import CustomUser
-from django.contrib.auth.admin import UserAdmin
-from django.forms import Textarea
-
-
 
 
 admin.site.register(CustomUser)
--- egyfund/urls.py
+++ egyfund/urls.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("accounts/", include("accounts.urls")),
