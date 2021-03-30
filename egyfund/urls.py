a613aca1b21b308b925388fcc61ba8191d44176c
ShehabEl-DeenAlalkamy <essamshehab33_3@h-eng.helwan.edu.eg>
2021-03-30 08:35:48 +0200

    including the funds.urls

--- egyfund/urls.py
+++ egyfund/urls.py
@@ -6,8 +6,10 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("accounts/", include("accounts.urls")),
+    path('', include('funds.urls')),
 ]
 
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
