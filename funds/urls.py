10336172809d34eb47eb2459d901cca1d2bbcd21
ahmedKhaled1995 <bigbossak84@gmail.com>
2021-03-31 12:43:41 +0200

    Profile page added

--- funds/urls.py
+++ funds/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import home, project
 
 urlpatterns = [
+    path('', home.index, name='egyfund'),
+    path('project/add', project.create, name='project_add'),
 
 ]
 
-
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
