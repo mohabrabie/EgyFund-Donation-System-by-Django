6a51ae558ade725886deeb4d9cba76d00a40c5a4
ahmedKhaled1995 <bigbossak84@gmail.com>
2021-03-31 12:58:37 +0200

    Added project start date and end date

--- funds/models/project.py
+++ funds/models/project.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django import forms
+
+
 from accounts.models import CustomUser
 from .category import Category
 from .tag import Tag
@@ -6,12 +9,21 @@ from .tag import Tag
 
 class Project(models.Model):
     title = models.CharField(max_length=100)
-    details = models.CharField(max_length=100)
+    details = models.TextField(max_length=100)
     is_featured = models.BooleanField(default=False)
-    # category = models.ForeignKey(Category, on_delete=models.CASCADE)  # Project can have ONE category
-    categories = models.ManyToManyField(Category)   #* Project can have MANY categories
+    categories = models.ManyToManyField(Category)   # Project can have MANY categories
+    start_date = models.DateField()
+    end_date = models.DateField()
     tags = models.ManyToManyField(Tag)  # * Project can have MANY tags
     user = models.ForeignKey(CustomUser, on_delete=models.NOT_PROVIDED)
 
     def __str__(self):
         return self.title
+
+    # Logic for raising error if end_date < start_date
+    def clean(self):
+        cleaned_data = super().clean()
+        start_date = cleaned_data.get("start_date")
+        end_date = cleaned_data.get("end_date")
+        if end_date < start_date:
+            raise forms.ValidationError("End date should be greater than start date.")
