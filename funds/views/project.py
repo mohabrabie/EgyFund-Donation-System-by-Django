e86797c950322352bbc85fa9718ada9ae696c209
ShehabEl-DeenAlalkamy <essamshehab33_3@h-eng.helwan.edu.eg>
2021-03-31 21:31:38 +0200

    User can now create a project

--- funds/views/project.py
+++ funds/views/project.py
@@ -1,7 +1,55 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.forms import modelformset_factory
+
+from funds.forms import ProjectForm, ProjectPictureForm
+from funds.models.project import Project
+from funds.models.projectPicture import ProjectPicture
 
 
+#TODO Find a way to give the user an option to add another image on demand & not restrict him to a no.
 @login_required
 def create(request):
-    pass
+    ProjectPictureFormSet = modelformset_factory(ProjectPicture, form=ProjectPictureForm, extra=3)
+
+    if request.method == 'POST':
+        project_form = ProjectForm(request.POST)
+        formset = ProjectPictureFormSet(request.POST, request.FILES, queryset=ProjectPicture.objects.none())
+
+        if project_form.is_valid() and formset.is_valid():
+            project_instance = project_form.save(commit=False)
+            project_instance.user = request.user
+            project_instance.save()
+
+            for form in formset.cleaned_data:
+                image = form['image']
+                project_picture_object = ProjectPicture(project=project_instance, image=image)
+                project_picture_object.save()
+            return redirect('myprojects')
+    else:
+        project_form = ProjectForm()
+        formset = ProjectPictureFormSet(queryset=ProjectPicture.objects.none())
+    return render(request, 'funds/add.html',
+                  {'project_form': project_form, 'formset': formset})
+
+
+#* This function including it's html is just for testing
+@login_required
+def show_all(request):
+    all_projects = Project.objects.filter(user=request.user)
+    return render(request, 'funds/myprojects.html', {'projects': all_projects})
+
+
+#! This method gives out an error: render() got an unexpected keyword argument 'renderer' & I am still investigating 
+# @login_required
+# def create(request):
+#     if request.method == "POST":
+#         project_form = ProjectForm(request.POST)
+#         if project_form.is_valid():
+#             project_instance = project_form.save(commit=False)
+#             project_instance.user = request.user
+#             project_instance.save()
+#             return redirect('myprojects')
+#     else:
+#         project_form = ProjectForm()
+#         return render(request, 'funds/add.html', {'form': project_form}, renderer=None)
