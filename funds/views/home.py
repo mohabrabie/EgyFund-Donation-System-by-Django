8b2b6ee1895581aec846197af1f130b9a95250d7
mohabrabie <mohabhamed2020@gmail.com>
2021-04-01 10:38:11 +0200

    added slid show in main page

--- funds/views/home.py
+++ funds/views/home.py
@@ -1,7 +1,37 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from funds.models.project import Project
+from funds.models.rating import Rating
+from funds.models.projectPicture import ProjectPicture
+
+from django.db.models import Sum
+from django.db.models import Max
 
 
 @login_required
 def index(request):
-    return render(request, 'funds/index.html')
+    projects_with_rating = Project.objects.all()
+    project_list = []
+    print(projects_with_rating)
+    for p in projects_with_rating:
+        rate = Rating.objects.filter(project=p).aggregate(Sum('rating'))
+        if rate['rating__sum'] == None:
+            rate['rating__sum'] = 0
+        img = ProjectPicture.objects.filter(project=p)[0]
+        dict = {
+            'project': p,
+            'rate': rate['rating__sum'],
+            'img': img
+        }
+        project_list.append(dict)
+    print(project_list)
+    print("=======================================================")
+    print(sorted(project_list, key=lambda i: i['rate'], reverse=True))
+    context = {
+        'projects': project_list,
+    }
+    return render(request, 'funds/home.html', context)
+
+
+def main(request):
+    return render(request, 'funds/home.html')
