1d9c3c2cb634fb78e01e0932c5b3fac8f29a6d5e
ahmedKhaled1995 <bigbossak84@gmail.com>
2021-04-01 16:04:03 +0200

    User can view has projects and donations (BETA)

--- accounts/urls.py
+++ accounts/urls.py
@@ -11,6 +11,8 @@ urlpatterns = [
     path("login/", views.login_user, name="login"),
     path("logout/", views.logout_user, name="logout"),
     path("profile/<int:user_id>", views.profile, name="profile"),
+    path("profile/<int:user_id>/projects", views.user_projects, name="accounts_projects"),
+    path("profile/<int:user_id>/donations", views.user_donations, name="accounts+donations"),
     path("activate/<uidb64>/<token>", views.verify, name='activate'),
     path("test/", views.test, name="test"),
 ]
