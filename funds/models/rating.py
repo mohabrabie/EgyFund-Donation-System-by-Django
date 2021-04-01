478b676fdd8e8377858415a74ade39c848913774
ShehabEl-DeenAlalkamy <essamshehab33_3@h-eng.helwan.edu.eg>
2021-04-01 21:03:39 +0200

    Added: Rating is cascade deleted as well

--- funds/models/rating.py
+++ funds/models/rating.py
@@ -7,8 +7,8 @@ from .project import Project
 class Rating(models.Model):
     rating = models.PositiveIntegerField(
         validators=[MinValueValidator(0), MaxValueValidator(5)])
-    project = models.ForeignKey(Project, on_delete=models.NOT_PROVIDED)
-    user = models.ForeignKey(CustomUser, on_delete=models.NOT_PROVIDED)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.project.__str__() + self.user.__str__() + str(self.rating)
