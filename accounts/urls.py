812f2af01f73800cf0c00971edf25ac394b820c8
ahmedKhaled1995 <bigbossak84@gmail.com>
2021-04-03 16:46:47 +0200

    Added Delete account (BETA)

--- accounts/urls.py
+++ accounts/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     path("logout/", views.logout_user, name="logout"),
     path("profile/<int:user_id>", views.profile, name="profile"),
     path("profile/<int:user_id>/projects", views.user_projects, name="accounts_projects"),
-    path("profile/<int:user_id>/donations", views.user_donations, name="accounts+donations"),
+    path("profile/<int:user_id>/donations", views.user_donations, name="accounts_donations"),
     path("activate/<uidb64>/<token>", views.verify, name='activate'),
     path("test/", views.test, name="test"),
 ]
