bcc7810db86015aad9fbc19f77e8f0f6f36415d4
ShehabEl-DeenAlalkamy <essamshehab33_3@h-eng.helwan.edu.eg>
2021-04-03 10:03:48 +0200

    Fixed add-image.js functionality

--- funds/static/js/add-image.js
+++ funds/static/js/add-image.js
@@ -1,25 +1,25 @@
-let birdForm = document.querySelectorAll(".bird-form")
-let container = document.querySelector("#form-container")
-let addButton = document.querySelector("#add-form")
-let totalForms = document.querySelector("#id_form-TOTAL_FORMS")
+let birdForm = document.querySelectorAll(".bird-form");
+let container = document.querySelector("#form-container");
+let addButton = document.querySelector("#add-form");
+let totalForms = document.querySelector("#id_form-TOTAL_FORMS");
+const MAX_IMAGES_NO = Number(addButton.getAttribute("max"));
 
-let formNum = birdForm.length - 1
-addButton.addEventListener('click', addForm)
+let formNum = birdForm.length - 1;
+addButton.addEventListener("click", addForm);
 
 function addForm(e) {
-    e.preventDefault()
+  e.preventDefault();
 
-    let newForm = birdForm[0].cloneNode(true)
-    let formRegex = RegExp(`form-(\\d){1}-`, 'g')
+  let newForm = birdForm[0].cloneNode(true);
+  let formRegex = RegExp(`form-(\\d){1}-`, "g");
 
-    formNum++
-    newForm.innerHTML = newForm.innerHTML.replace(formRegex, `form-${formNum}-`)
-    container.insertBefore(newForm, addButton)
+  formNum++;
+  newForm.innerHTML = newForm.innerHTML.replace(formRegex, `form-${formNum}-`);
+  container.insertBefore(newForm, addButton);
 
-    totalForms.setAttribute('value', `${formNum+1}`)
-   
+  totalForms.setAttribute("value", `${formNum + 1}`);
 
-    if (formNum > (Number("{{ formset.max_num }}") - 2)) {
-        addButton.style.display = "none"
-    }
+  if (formNum > MAX_IMAGES_NO - 2) {
+    addButton.style.display = "none";
+  }
 }
