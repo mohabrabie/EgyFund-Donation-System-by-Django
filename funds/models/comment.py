63fe8f514f2a65aa6b5f744fc2da88499ca54a40
ShehabEl-DeenAlalkamy <essamshehab33_3@h-eng.helwan.edu.eg>
2021-04-03 20:11:54 +0200

    Adding created_at field to Comment, CommentReport & ProjectReport + __str__() is now more descriptive

--- funds/models/comment.py
+++ funds/models/comment.py
@@ -1,12 +1,17 @@
 from django.db import models
 from accounts.models import CustomUser
 from .project import Project
+from django.utils import timezone
 
 
 class Comment(models.Model):
     comment = models.CharField(max_length=100)
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(default=timezone.now)
+    
+    def formatted_date(self):
+        return self.created_at.strftime("%h %d, %Y at %H:%M")
 
     def __str__(self):
-        return self.project.__str__() + self.user.__str__() + self.comment
+        return f"{self.user.username} commented on {self.project} project on {self.formatted_date()}"
--- funds/models/projectReport.py
+++ funds/models/projectReport.py
@@ -1,12 +1,17 @@
 from django.db import models
 from accounts.models import CustomUser
 from .project import Project
+from django.utils import timezone
 
 
 class ProjectReport(models.Model):
     report = models.CharField(max_length=100)
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(default=timezone.now)
+    
+    def formatted_date(self):
+        return self.created_at.strftime("%h %d, %Y at %H:%M")
 
     def __str__(self):
-        return self.project.__str__() + ", " + self.report
+        return f"{self.user.username} reported {self.comment.project} project on {self.formatted_date()}"
