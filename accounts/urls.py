0981bf07d3121c77fc32048bb8662d1dcea6870c
ahmedKhaled1995 <bigbossak84@gmail.com>
2021-04-04 15:21:19 +0200

    Finished user deletion

--- accounts/urls.py
+++ accounts/urls.py
@@ -14,6 +14,7 @@ urlpatterns = [
     path("profile/<int:user_id>/projects", views.user_projects, name="accounts_projects"),
     path("profile/<int:user_id>/donations", views.user_donations, name="accounts_donations"),
     path("profile/<int:user_id>/delete", views.profile_delete, name="accounts_delete"),
+    path("profile/<int:user_id>/delete/confirm", views.profile_delete_confirm, name="accounts_delete_confirm"),
     path("forbidden/", views.forbidden, name="forbidden"),
     path("activate/<uidb64>/<token>", views.verify, name='activate'),
     path("test/", views.test, name="test"),
