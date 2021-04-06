ca1df69886e67e5b41393d4803805c9ac3cf6664
zak <ahmedzak19972008@gmail.com>
2021-04-06 18:19:20 -0400

    Code refactoring

--- funds/views/home.py
+++ funds/views/home.py
@@ -6,7 +6,6 @@ from funds.models.projectPicture import ProjectPicture
 from funds.models.category import Category
 from django.db.models import Sum
 from django.db.models import Avg
-from django.http import JsonResponse
 
 
 def get_all_data():
